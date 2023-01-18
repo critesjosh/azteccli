788cb11249400209cd299a5da40a9f62141a2268
Josh Crites <jc@joshcrites.com>
2023-01-18 10:43:28 -0500

    update

--- src/network_config.ts
+++ src/network_config.ts
@@ -12,9 +12,17 @@ let networkConfig: Config = {
     rollupProvider: "https://api.aztec.network/aztec-connect-prod/falafel",
     explorerUrl: "https://aztec-connect-prod-explorer.aztec.network/",
   },
+  // 3567: {
+  //   rollupProvider: "https://api.aztec.network/aztec-connect-dev/falafel",
+  //   explorerUrl: "https://aztec-connect-dev-explorer.aztec.network/",
+  // },
   3567: {
-    rollupProvider: "https://api.aztec.network/aztec-connect-dev/falafel",
-    explorerUrl: "https://aztec-connect-dev-explorer.aztec.network/",
+    rollupProvider: "http://localhost:8081",
+    explorerUrl: "",
+  },
+  31337: {
+    rollupProvider: "http://localhost:8081",
+    explorerUrl: ""
   },
   677868: {
     rollupProvider: "https://api.aztec.network/aztec-connect-testnet/falafel/",
